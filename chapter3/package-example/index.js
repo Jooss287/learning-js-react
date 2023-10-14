3fcea1b4d648361dae5645fd25bc07843a00c22a
jooss287 <jooss287@gmail.com>
2023-10-14 23:22:46 +0900

    learn-js-react 강의
    * 2023-10-14
    * Node.js 관련 내용
        * 설치, module, npm 등
    * `create-react-app` 세팅

---
+++ chapter3/package-example/index.js
@@ -0,0 +1,5 @@
+const randomColor = require('randomcolor'); // import the script
+var color = randomColor(); // a hex code for an attractive color
+
+console.log("hello nodejs package");
+console.log(color);
