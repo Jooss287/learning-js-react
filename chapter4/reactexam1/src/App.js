00362510c55bd12fee48d4309e682ad5f122477d
jooss287 <jooss287@gmail.com>
2023-10-19 22:38:41 +0900

    learn-js-react 강의
    * 2023-10-19
    * React 기능 소개
        * JSX(MyHeader, MyFooter)
        * State(Counter)
        * Props(OddEvenResult, Container)

--- chapter4/reactexam1/src/App.js
+++ chapter4/reactexam1/src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
+import Container from './Container';
+import Counter from './Counter';
+import MyHeader from './MyHeader'
+// import MyFooter from './MyFooter'
 
 function App() {
+  const style ={
+    inline_style: {
+      color: "blue",
+    },
+  };
+
+  const func = () => {
+    return 'func'
+  };
+  const number = 5;
+
+  const counterProps ={
+    a: 1,
+    b: 2,
+    c: 3,
+    initialValue: 5,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Hello react </h2>
-      </header>
+    //최상위 태그가 항상 필요
+    <div className="App"> 
+      <Container>
+      <MyHeader />
+        {/*중괄호에는 문자, 숫자가 들어 갈 수 있음 */}
+        <h2>Hello react {func()}</h2>
+        <b id="bold_text">React.js</b>
+        <b style={style.inline_style} id="inline">React.js</b>
+        <b id="bold_text">{number}는 : {number % 2 === 0 ? "짝수" : "홀수"}</b>
+
+        <Counter {...counterProps}/>
+        </Container>
     </div>
   );
 }
---
+++ chapter4/reactexam1/src/Counter.js
@@ -0,0 +1,31 @@
+import React,{useState} from 'react';
+import OddEvenResult from './OddEventResult';
+
+const Counter = ({initialValue}) => {
+    // 0에서 출발
+    // 1씩 증가하고
+    // 1씩 감소하는
+    // count 상태
+
+    const [count, setCount] = useState(initialValue);
+    const onIncrease = () => {
+        setCount(count + 1);
+    }
+    const onDecrease = () => {
+        setCount(count - 1);
+    }
+    return (
+        <div>
+            <h2>{count}</h2>
+            <button onClick={onIncrease}>+</button>
+            <button onClick={onDecrease}>-</button>
+            <OddEvenResult count={count}/>
+        </div>
+    )
+};
+
+Counter.props = {
+    initialValue: 0,
+};
+
+export default Counter;
