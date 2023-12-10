86e305267ef370e15ac23b348a1fce4ab5323589
jooss287 <jooss287@gmail.com>
2023-12-10 23:34:35 +0900

    간단한 다이어리 만들기 4강
    * 2023-12-10
    * Lifecycle 확인
    * api로 데이터 받아오기

--- chapter5/simple-diary/src/App.js
+++ chapter5/simple-diary/src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
+import Lifecycle from './Lifecycle';
+
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
 
   // state 끌어올리기
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
+    
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      }
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -38,6 +61,7 @@ function App() {
 
   return (
     <div className="App">
+      {/* <Lifecycle /> */}
       <DiaryEditor onCreate={onCreate}/>
       <DiaryList onEdit={onEdit} onDelete={onRemove} diaryList={data}/>
     </div>
